7f7d58c6f1b829a150576dfef7fa359891218365
Qinen Zhu <qinzhu@microsoft.com>
2016-08-08 09:09:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub Activity Enhancement
+
+By default, your github home page only show others activities. If you want to view your own activities, you need open https://github.com/username?tab=activity.
+
+Using this chrome extension, you github home page will merge the default activities and your own activities ordered by time.
---
+++ index.js
@@ -0,0 +1,71 @@
+$(function () {
+  var baseUrl = "https://github.com/";
+  Promise.all([getSelfActivity(), getOthersActivity()]).then(mergeActivity).catch(function(error) {
+    console.log(error);
+  });
+
+  function mergeActivity(activityArr) {
+    if (activityArr.length !== 2) {
+      console.log("Error occurred when run mergeActivity, invalid parameter");
+      return;
+    }
+    var selfActivity = activityArr[0];
+    var othersActivity = activityArr[1];
+    var index = 0;
+    othersActivity.forEach(function (item) {
+      while (index < selfActivity.length && selfActivity[index].time > item.time) {
+        item.element.before(selfActivity[index].html);
+        index = index + 1;
+      }
+    });
+  }
+
+  function getOthersActivity() {
+    return new Promise(function (resolve, reject) {
+      var activities = [];
+      $(".alert").each(function () {
+        var item = {};
+        var el = $(this);
+        var el_time = el.find(".body>.time>relative-time").attr('datetime') ||
+          el.find(".body>.simple>.time>relative-time").attr('datetime');
+        item.time = el_time;
+        item.element = el;
+        activities.push(item);
+      });
+      resolve(activities);
+    });
+  }
+
+  function getSelfActivity() {
+    var selfAtomUrl = baseUrl + $("meta[name='user-login']").attr('content') + ".atom";
+    return new Promise(function (resolve, reject) {
+      getSelfAtom(selfAtomUrl)
+      .then(function (xml) {
+        var selfActivity = transformXML(xml);
+        resolve(selfActivity);
+      })
+      .catch(reject)
+    });
+  }
+
+  function getSelfAtom(url) {
+    return new Promise(function (resolve, reject) {
+      $.get(url, function (data) {
+        resolve(data);
+      }).fail(reject);
+    });
+  }
+
+  function transformXML(xml) {
+    var entryList = [];
+    $(xml).find("entry").each(function () {
+      var item = {};
+      var content = $("<div/>").html($(this).find("content")).text();
+      item.html = $("<div class='alert'/>").append($("<div class='body'/>").append(content))[0].outerHTML;
+      item.time = $(this).find("updated").text();
+      entryList.push(item);
+    });
+    return entryList;
+  }
+});
+
